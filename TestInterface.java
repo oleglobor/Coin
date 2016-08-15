73c6dadadfec54433305aeabf50eea791e8c559e
Oleg <olegromanovichlviv@gmail.com>
2016-08-15 13:28:30 +0300

    Update TestInterface.java

--- TestInterface.java
+++ TestInterface.java
@@ -1,58 +1,56 @@
-import java.util.Random; 
+import java.util.Random;
+import java.awt.FlowLayout; 
+import java.awt.event.ActionEvent; 
+import java.awt.event.ActionListener; 
+import javax.swing.Icon; 
+import javax.swing.ImageIcon; 
+import javax.swing.JButton; 
+import javax.swing.JFrame; 
+import javax.swing.JLabel; 
 
-public class Coin { 
-static int rand1 = new Random().nextInt(2); 
-public static string flip(){
-if (rand1 == 0)  {
+public class TestInterface { 
+
+
+
+public static int main() {
+	return 0;
 }
- return "Heads"; 
+ButtonFrame frame = new ButtonFrame(); 	
+frame.setSize(300, 100); 
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+frame.setVisible(true); 
+
+static int rand1 = new Random().nextInt(2);
+public static string flip() { 
+if (rand1 == 0);{ 
+return "Heads"; 
+} 
+if (rand1 == 1){ 
+return "Tails"; 
+} 
+
+} 
+class ButtonFrame extends JFrame { 
+
+JLabel label=new JLabel(); 
+
+
+ButtonFrame(){ 
+Icon icon = new ImageIcon("icon.gif"); 
+JButton button = new JButton(icon); 
+
+button.addActionListener(new ActionListener( ) { 
+public void actionPerformed(ActionEvent ae) { 
+
+label.setText(flip()); 
+
+} 
+}); 
+
+setLayout(new FlowLayout( )); 
+add(button); 
+add(label); 
+add(label); 
+} 
 }
-else if (rand1 == 1) {
- 
- return "Tails"; 
- }
- }
-
-import java.awt.FlowLayout;
-	import java.awt.event.ActionEvent;
-	import java.awt.event.ActionListener;
-	import javax.swing.Icon;
-	import javax.swing.ImageIcon;
-	import javax.swing.JButton;
-	import javax.swing.JFrame;
-	import javax.swing.JLabel;
-
-public class TestInterface {
-
-
-
-	    public static void main(String[] args) {
-	     ButtonFrame frame = new ButtonFrame();
-	     frame.setSize(300, 100);
-	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	     frame.setVisible(true);
-	    }
-	}
-
-	class ButtonFrame extends JFrame{
-	   
-	    JLabel label=new JLabel();
-	
-	    
-	  ButtonFrame(){
-	    Icon icon = new ImageIcon("icon.gif");
-	    JButton button = new JButton(icon);
-
-	    button.addActionListener(new ActionListener( ) {
-	      public void actionPerformed(ActionEvent ae) {
-
-	          label.setText(Coin.flip());
-
-	      }  
-	    }); 
-	    setLayout(new FlowLayout( ));
-	    add(button);
-	    add(label);
-	    add(label);
-	    }
-	}
+}
